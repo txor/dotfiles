323097204e314a5418860e7b7a60b331816c7a8a
Jordi 'Txor' Casas Ríos <jordi.casas@edreamsodigeo.com>
2020-05-20 14:16:31 +0200

    Added powerline fontconfig setup on .dotfiles/powerline_install.sh

--- .dotfiles/powerline_install.sh
+++ .dotfiles/powerline_install.sh
@@ -1,3 +1,12 @@
 # From https://powerline.readthedocs.io/en/latest/installation.html#repository-root
 pip install --user --editable=$HOME/.dotfiles_gitsubmodules/powerline
 ln -s $HOME/.dotfiles_gitsubmodules/powerline/scripts/powerline ~/.local/bin
+
+# From https://powerline.readthedocs.io/en/master/installation/linux.html#fontconfig
+mkdir -p ~/.local/share/fonts/
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+fc-cache -vf ~/.local/share/fonts
+mkdir -p ~/.config/fontconfig/conf.d/
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
